5cd60d4f7d9e2d2c6897c9c9243cff79c2eaf011
Jorge Medina <jlmedinamacias@gmail.com>
2024-09-15 14:25:16 -0700

    Add user authentication functionallity.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_09_04_185003) do
+ActiveRecord::Schema.define(version: 2024_09_15_204153) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,8 @@ ActiveRecord::Schema.define(version: 2024_09_04_185003) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
   end
 
   add_foreign_key "line_items", "orders"
