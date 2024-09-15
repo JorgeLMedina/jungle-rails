133c284ea04c202048e2f346d21e226960151bf4
Jorge Medina <jlmedinamacias@gmail.com>
2024-09-15 13:32:04 -0700

    Add Sign Up and Sign In pages succesfully, functionality still to be tested

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   # Validations
+  validates :first_name, presence: true
+  validates :last_name, presence: true
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, length: { minimum: 6 }
 
